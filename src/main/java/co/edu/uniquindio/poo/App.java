d5541fd94d1784cfc04f30ba47eb3176b9dd0d70
Jurden <daniele.juradoc@uqvirtual.edu.co>
2024-02-24 11:12:06 -0500

    cuerpo clases

---
+++ src/main/java/co/edu/uniquindio/poo/App.java
@@ -0,0 +1,12 @@
+package co.edu.uniquindio.poo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        private int num = 5; 
+        System.out.println("Hello World!");
+    }
+}
